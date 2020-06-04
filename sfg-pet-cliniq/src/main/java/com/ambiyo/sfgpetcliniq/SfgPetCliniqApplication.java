3bfca145108dd0599e48e5db9e9721e9ceda32e3
ambiyo-marion <ambiyomarion@gmail.com>
2020-06-04 09:32:07 +0300

    Initial project add

---
+++ sfg-pet-cliniq/src/main/java/com/ambiyo/sfgpetcliniq/SfgPetCliniqApplication.java
@@ -0,0 +1,13 @@
+package com.ambiyo.sfgpetcliniq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetCliniqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetCliniqApplication.class, args);
+	}
+
+}
