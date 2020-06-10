42ca82c3d93cafa8f5b72117ca31c3bdbaf14ea3
ambiyo-marion <ambiyomarion@gmail.com>
2020-06-10 11:45:03 +0300

    initial project add

---
+++ Spring5-jokes-application/src/test/java/com/ambiyo/Spring5jokesapplication/Spring5JokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambiyo.Spring5jokesapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
